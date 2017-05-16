95c19a28263a3645f588425984a6ee675d1fe002
Tamaized <ryanolt123@gmail.com>
2017-05-15 17:54:08 -0700

    cherry-pick: fixed a retarded crash

--- src/main/java/Tamaized/AoV/events/TickHandler.java
+++ src/main/java/Tamaized/AoV/events/TickHandler.java
@@ -7,6 +7,9 @@ import java.util.UUID;
 import Tamaized.AoV.AoV;
 import Tamaized.AoV.capabilities.CapabilityList;
 import Tamaized.AoV.capabilities.aov.IAoVCapability;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.MobEffects;
+import net.minecraft.potion.PotionEffect;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.TickEvent.PlayerTickEvent;
 
@@ -15,9 +18,15 @@ public class TickHandler {
 	@SubscribeEvent
 	public void update(PlayerTickEvent e) {
 		if (e.phase == e.phase.START) return;
-		IAoVCapability cap = e.player.getCapability(CapabilityList.AOV, null);
+		EntityPlayer player = e.player;
+		if (player.getHealth() <= (player.getMaxHealth() / 2) && player.getActivePotionEffect(AoV.potions.stalwartPact) != null) {
+			player.removeActivePotionEffect(AoV.potions.stalwartPact);
+			player.addPotionEffect(new PotionEffect(MobEffects.ABSORPTION, (20 * (60 * 5)), 2));
+			player.addPotionEffect(new PotionEffect(MobEffects.REGENERATION, (20 * (10)), 2));
+		}
+		IAoVCapability cap = player.getCapability(CapabilityList.AOV, null);
 		if (cap != null) {
-			cap.update(e.player);
+			cap.update(player);
 		}
 	}
 
