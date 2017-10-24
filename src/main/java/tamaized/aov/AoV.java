300f4c128b2293e3954c6943b5bd5c72b1c4c8c0
Tamaized <ryanolt123@gmail.com>
2017-10-24 13:58:51 -0700

    apparently i never pushed this but uh, build ver bump

--- src/main/java/tamaized/aov/AoV.java
+++ src/main/java/tamaized/aov/AoV.java
@@ -36,7 +36,7 @@ import tamaized.tammodized.TamModBase;
 import tamaized.tammodized.TamModized;
 import tamaized.tammodized.proxy.AbstractProxy;
 
-@Mod(modid = AoV.modid, name = "Angel of Vengeance", version = AoV.version, dependencies = "required-before:" + TamModized.modid + "@[${tamversion},)")
+@Mod(modid = AoV.modid, name = "Angel of Vengeance", version = AoV.version, acceptedMinecraftVersions = "[1.12,)", dependencies = "required-before:" + TamModized.modid + "@[${tamversion},)")
 public class AoV extends TamModBase {
 
 	public static final String modid = "aov";
