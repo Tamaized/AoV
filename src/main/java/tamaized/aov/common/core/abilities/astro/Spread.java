3d0140fc930d38efc8a9c34fa6abe36fb2980160
Tamaized <ryanolt123@gmail.com>
2019-03-11 00:05:54 -0400

    forgot to fix spread as well, #114

--- src/main/java/tamaized/aov/common/core/abilities/astro/Spread.java
+++ src/main/java/tamaized/aov/common/core/abilities/astro/Spread.java
@@ -80,7 +80,7 @@ public class Spread extends AbilityBase {
 
 	@Override
 	public boolean isCastOnTarget(EntityPlayer caster, IAoVCapability cap, EntityLivingBase target) {
-		return false;
+		return IAoVCapability.canBenefit(caster, cap, target);
 	}
 
 	@Override
@@ -102,7 +102,7 @@ public class Spread extends AbilityBase {
 			}
 		} else {
 			IAstroCapability.ICard card = astro.getSpread();
-			EntityLivingBase entity = target == null || caster.getDistance(target) < getMaxDistance() ? caster : target;
+			EntityLivingBase entity = target == null || caster.getDistance(target) >= getMaxDistance() ? caster : target;
 			int potency = (int) Math.floor(aov.getSpellPower() / 10F);
 			IAstroCapability.ICard burn = astro.getBurn();
 			astro.useSpread(caster);
