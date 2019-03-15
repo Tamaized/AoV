b416739f654df9f9b28f9c511f29cf4c39fea59c
Tamaized <ryanolt123@gmail.com>
2019-03-15 18:36:44 -0400

    E

--- src/main/java/tamaized/aov/client/gui/GuiScreenClose.java
+++ src/main/java/tamaized/aov/client/gui/GuiScreenClose.java
@@ -3,19 +3,18 @@ package tamaized.aov.client.gui;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiButton;
 import net.minecraft.client.gui.GuiScreen;
-import net.minecraft.client.util.InputMappings;
 
 public class GuiScreenClose extends GuiScreen {
 
 	@Override
-	public boolean charTyped(char typedChar, int keyCode) {
-		if (keyCode == 1 || this.mc.gameSettings.keyBindInventory.isActiveAndMatches(InputMappings.getInputByCode(keyCode, 0))) {
+	public boolean keyReleased(int key, int scan, int mods) {
+		if (mc.gameSettings.keyBindInventory.getKey().getKeyCode() == key) {
 			if (mc.player == null)
 				Minecraft.getInstance().displayGuiScreen(null);
 			else
 				this.mc.player.closeScreen();
 		}
-		return super.charTyped(typedChar, keyCode);
+		return super.keyReleased(key, scan, mods);
 	}
 
 	@Override
