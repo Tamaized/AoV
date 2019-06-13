e0b07e081e4785308af4e0ff18d3d7202209d454
Tamaized <ryanolt123@gmail.com>
2019-06-12 20:54:37 -0400

    temp stencil buffer workaround

--- src/main/java/tamaized/aov/client/ClientHelpers.java
+++ src/main/java/tamaized/aov/client/ClientHelpers.java
@@ -1,7 +1,11 @@
 package tamaized.aov.client;
 
 import com.google.common.collect.Sets;
+import com.mojang.blaze3d.platform.GLX;
+import com.mojang.blaze3d.platform.GlStateManager;
+import com.mojang.blaze3d.platform.TextureUtil;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.shader.Framebuffer;
 import net.minecraft.entity.Entity;
 import net.minecraft.util.math.RayTraceResult;
 import tamaized.aov.common.helper.RayTraceHelper;
@@ -12,10 +16,11 @@ public final class ClientHelpers {
 
 	private static final HashSet<Entity> BLANK_SET = Sets.newHashSet();
 
+	private static boolean stencil = false;
+
 	private ClientHelpers() {
 
 	}
-
 
 	public static Entity getTargetOverMouse(Minecraft mc, int range) {
 		BLANK_SET.clear();
@@ -23,6 +28,48 @@ public final class ClientHelpers {
 		RayTraceResult ray = RayTraceHelper.tracePath(mc.world, mc.player, range, 1, BLANK_SET);
 		BLANK_SET.clear();
 		return ray == null || ray.type != RayTraceResult.Type.ENTITY ? null : ray.entity;
+	}
+
+	public static boolean isStencilBufferEnabled() {
+		return stencil;
+	}
+
+	public static void enableStencilBuffer() { // TODO remove when/if Framebuffer gets a method for this again
+		if (stencil)
+			return;
+		stencil = true;
+		Framebuffer fbo = Minecraft.getInstance().getFramebuffer();
+		int width = fbo.framebufferWidth;
+		int height = fbo.framebufferHeight;
+		fbo.framebufferWidth = width;
+		fbo.framebufferHeight = height;
+		fbo.framebufferTextureWidth = width;
+		fbo.framebufferTextureHeight = height;
+		if (!GLX.isUsingFBOs()) {
+			fbo.func_216493_b(Minecraft.IS_RUNNING_ON_MAC);
+		} else {
+			fbo.framebufferObject = GLX.glGenFramebuffers();
+			fbo.framebufferTexture = TextureUtil.generateTextureId();
+			if (fbo.useDepth) {
+				fbo.depthBuffer = GLX.glGenRenderbuffers();
+			}
+
+			fbo.setFramebufferFilter(9728);
+			GlStateManager.bindTexture(fbo.framebufferTexture);
+			GlStateManager.texImage2D(3553, 0, 32856, fbo.framebufferTextureWidth, fbo.framebufferTextureHeight, 0, 6408, 5121, null);
+			GLX.glBindFramebuffer(GLX.GL_FRAMEBUFFER, fbo.framebufferObject);
+			GLX.glFramebufferTexture2D(GLX.GL_FRAMEBUFFER, GLX.GL_COLOR_ATTACHMENT0, 3553, fbo.framebufferTexture, 0);
+			if (fbo.useDepth) {
+				GLX.glBindRenderbuffer(GLX.GL_RENDERBUFFER, fbo.depthBuffer);
+				GLX.glRenderbufferStorage(GLX.GL_RENDERBUFFER, org.lwjgl.opengl.EXTPackedDepthStencil.GL_DEPTH24_STENCIL8_EXT, fbo.framebufferTextureWidth, fbo.framebufferTextureHeight);
+				GLX.glFramebufferRenderbuffer(GLX.GL_FRAMEBUFFER, org.lwjgl.opengl.EXTFramebufferObject.GL_DEPTH_ATTACHMENT_EXT, GLX.GL_RENDERBUFFER, fbo.depthBuffer);
+				GLX.glFramebufferRenderbuffer(GLX.GL_FRAMEBUFFER, org.lwjgl.opengl.EXTFramebufferObject.GL_STENCIL_ATTACHMENT_EXT, GLX.GL_RENDERBUFFER, fbo.depthBuffer);
+			}
+
+			fbo.checkFramebufferComplete();
+			fbo.func_216493_b(Minecraft.IS_RUNNING_ON_MAC);
+			fbo.unbindFramebufferTexture();
+		}
 	}
 
 }
