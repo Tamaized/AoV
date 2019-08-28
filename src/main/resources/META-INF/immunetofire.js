dc3f99411aee36b99f594ba3c61ed9f23a962716
Tamaized <ryanolt123@gmail.com>
2019-08-28 00:57:26 -0400

    no u

--- src/main/resources/META-INF/immunetofire.js
+++ src/main/resources/META-INF/immunetofire.js
@@ -8,14 +8,14 @@ function initializeCoreMod() {
                 'methodDesc': '()Z'
             },
             'transformer': function (methodNode) {
-                if (methodNode instanceof org.objectweb.asm.tree.MethodNode) { // Stupid way to cast in JS
+                if (methodNode instanceof org.objectweb.asm.tree.MethodNode) { // Stupid way to cast in JS to avoid warnings and fix autocomplete
                     var ASM = Java.type('net.minecraftforge.coremod.api.ASMAPI');
                     var Opcodes = Java.type('org.objectweb.asm.Opcodes');
                     methodNode.instructions.insertBefore(
                         ASM.findFirstInstruction(methodNode, Opcodes.IRETURN),
                         ASM.listOf(
                             new org.objectweb.asm.tree.VarInsnNode(Opcodes.ALOAD, 0), // PUSH this TO THE TOP OF THE STACK
-                            new org.objectweb.asm.tree.MethodInsnNode( // INVOKE tamaized.aov.asm.ASMHooks#isImmuneToFire(Entity, boolean)
+                            new org.objectweb.asm.tree.MethodInsnNode( // INVOKE tamaized.aov.asm.ASMHooks#isImmuneToFire(boolean, Entity)
                                 Opcodes.INVOKESTATIC,
                                 'tamaized/aov/asm/ASMHooks',
                                 'isImmuneToFire',
