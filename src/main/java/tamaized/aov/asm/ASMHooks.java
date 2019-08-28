1187f026897e871dc4ce59f9dc71ca2bd3909d84
Tamaized <ryanolt123@gmail.com>
2019-08-28 02:16:32 -0400

    check useDepth you dumbass

--- src/main/java/tamaized/aov/asm/ASMHooks.java
+++ src/main/java/tamaized/aov/asm/ASMHooks.java
@@ -25,11 +25,14 @@ public class ASMHooks {
 	 * {@link net.minecraft.client.shader.Framebuffer#func_216492_b}<br>
 	 * [BEFORE] {@link net.minecraft.client.shader.Framebuffer#checkFramebufferComplete}
 	 */
-	public static void enableStencilBuffer(Framebuffer fbo){
-		GLX.glBindRenderbuffer(GLX.GL_RENDERBUFFER, fbo.depthBuffer);
-		GLX.glRenderbufferStorage(GLX.GL_RENDERBUFFER, org.lwjgl.opengl.EXTPackedDepthStencil.GL_DEPTH24_STENCIL8_EXT, fbo.framebufferTextureWidth, fbo.framebufferTextureHeight);
-		GLX.glFramebufferRenderbuffer(GLX.GL_FRAMEBUFFER, org.lwjgl.opengl.EXTFramebufferObject.GL_DEPTH_ATTACHMENT_EXT, GLX.GL_RENDERBUFFER, fbo.depthBuffer);
-		GLX.glFramebufferRenderbuffer(GLX.GL_FRAMEBUFFER, org.lwjgl.opengl.EXTFramebufferObject.GL_STENCIL_ATTACHMENT_EXT, GLX.GL_RENDERBUFFER, fbo.depthBuffer);
+	@SuppressWarnings("unused")
+	public static void enableStencilBuffer(Framebuffer fbo) {
+		if (fbo.useDepth) {
+			GLX.glBindRenderbuffer(GLX.GL_RENDERBUFFER, fbo.depthBuffer);
+			GLX.glRenderbufferStorage(GLX.GL_RENDERBUFFER, org.lwjgl.opengl.EXTPackedDepthStencil.GL_DEPTH24_STENCIL8_EXT, fbo.framebufferTextureWidth, fbo.framebufferTextureHeight);
+			GLX.glFramebufferRenderbuffer(GLX.GL_FRAMEBUFFER, org.lwjgl.opengl.EXTFramebufferObject.GL_DEPTH_ATTACHMENT_EXT, GLX.GL_RENDERBUFFER, fbo.depthBuffer);
+			GLX.glFramebufferRenderbuffer(GLX.GL_FRAMEBUFFER, org.lwjgl.opengl.EXTFramebufferObject.GL_STENCIL_ATTACHMENT_EXT, GLX.GL_RENDERBUFFER, fbo.depthBuffer);
+		}
 	}
 
 }
